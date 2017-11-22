0cc0b401b51919e8f69875f1c7ac63e626c39f1b
renjithspace <renjithrajeevvk@gmail.com>
2017-11-22 07:27:46 +0530

    Code format

--- src/js/offline.js
+++ src/js/offline.js
@@ -23,68 +23,68 @@
         }
 
         /**
-         *----------------
-         * Private Methods
-         *----------------
-         *
-         */
+        *----------------
+        * Private Methods
+        *----------------
+        *
+        */
 
-         var self = this;
+        var self = this;
 
-         // Block
-         var block = function() {
+        // Block
+        var block = function() {
 
-             // Create blocker wrapper
-             var blocker = document.createElement('div');
-             blocker.className = 'offline-blocker';
+            // Create blocker wrapper
+            var blocker = document.createElement('div');
+            blocker.className = 'offline-blocker';
 
-             // Create notification
-             var notify = document.createElement('p');
-             notify.innerHTML = self.options.notify;
-             blocker.appendChild(notify);
+            // Create notification
+            var notify = document.createElement('p');
+            notify.innerHTML = self.options.notify;
+            blocker.appendChild(notify);
 
-             // Append
-             if (!document.querySelectorAll('.offline-blocker').length) {
-                 document.body.appendChild(blocker);
-             }
-         };
+            // Append
+            if (!document.querySelectorAll('.offline-blocker').length) {
+                document.body.appendChild(blocker);
+            }
+        };
 
-         // Unblock
-         var unblock = function() {
+        // Unblock
+        var unblock = function() {
 
-             // Remove blocker
-             var blocker = document.querySelector('.offline-blocker');
-             if (blocker) {
-                 blocker.parentNode.removeChild(blocker);
-             }
-         };
+            // Remove blocker
+            var blocker = document.querySelector('.offline-blocker');
+            if (blocker) {
+                blocker.parentNode.removeChild(blocker);
+            }
+        };
 
-         // Check
-         var check = function() {
+        // Check
+        var check = function() {
 
-             if (navigator.onLine) {
+            if (navigator.onLine) {
 
-                 // Make AJAX request
-                 var request = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
-                 var rand = Math.floor((1 + Math.random()) * 1000);
-                 request.open( "HEAD", "//" + window.location.hostname + "/?rand=" + rand, false );
+                // Make AJAX request
+                var request = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
+                var rand = Math.floor((1 + Math.random()) * 1000);
+                request.open( "HEAD", "//" + window.location.hostname + "/?rand=" + rand, false );
 
-                 // Try request status
-                 try {
-                     request.send();
-                     if (request.status == 200) {
-                         unblock();
-                     }
-                 } catch (error) {
-                     block();
-                 }
-             } else {
-                 block();
-             }
+                // Try request status
+                try {
+                    request.send();
+                    if (request.status == 200) {
+                        unblock();
+                    }
+                } catch (error) {
+                    block();
+                }
+            } else {
+                block();
+            }
 
-         };
+        };
 
-         // Invoke
-         setInterval(check, this.options.timeout);
+        // Invoke
+        setInterval(check, this.options.timeout);
     };
 }());
