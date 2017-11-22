09d77f2ed989cd743ca39383bdf706ebe562a2cf
renjithspace <renjithrajeevvk@gmail.com>
2017-11-22 07:08:48 +0530

    Extend default options

--- src/js/offline.js
+++ src/js/offline.js
@@ -7,5 +7,19 @@
             notify: 'Opps! No internet connection',
             timeout: 10000,
         };
+
+        // Extend default options
+        if (arguments.length && typeof arguments[0] === 'object') {
+            setOptions(this.options, arguments[0]);
+        }
+
+        // Set options
+        function setOptions(options, args) {
+            for (let arg in args) {
+                if (options.hasOwnProperty(arg)) {
+                    options[arg] = args[arg];
+                }
+            }
+        }
     };
 }());
